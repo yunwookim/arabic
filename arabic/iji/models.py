d3078cc6281e94adb0b1ead30d03c9d349dfbcb9
genesis <genesis041835@gmail.com>
2015-02-04 16:40:44 +0900

    model2

--- arabic/iji/models.py
+++ arabic/iji/models.py
@@ -1,7 +1,7 @@
-
 from django.contrib.auth.models import User
 from django.db import models
 import json
+
 
 # Create your models here.
 class Company(models.Model):
@@ -14,14 +14,17 @@ class Company(models.Model):
     def get_count(self):
         return self.user_count
 
+
 class Class(models.Model):
     code = models.CharField()
     name = models.CharField()
 
     def __str__(self):
         return self.name
+
     def get_code(self):
         return self.code
+
 
 class Lecture(models.Model):
     name = models.CharField()
@@ -32,30 +35,37 @@ class Lecture(models.Model):
 
     def __str__(self):
         return self.name
+
     def get_type(self):
         return self.lec_type
+
     def get_video(self):
         return self.video
+
     def get_audio(self):
         return self.audio
+
     def get_code(self):
         return self.lec_code
 
 
-
 class Student(models.Model):
     user = models.OneToOneField(User)
-    curlec = models.CharField()
+    curLec = models.CharField()
     phone = models.CharField(max_length=20)
 
     def __str__(self):
         return self.get_name() + "(" + self.user.get_username() + ")"
+
     def get_name(self):
         return self.user.last_name + self.user.first_name
+
     def set_curlec(self, x):
-        self.curlec = json.dumps(x)
+        self.curLec = json.dumps(x)
+
     def get_curlec(self):
-        return json.load(self.curlec)
+        return json.load(self.curLec)
+
     def get_phone(self):
         return self.phone
 
